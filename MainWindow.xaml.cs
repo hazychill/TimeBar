81bd4fc3ed60784ac3fb1da3fc9b04e45073f800
Hiroki Murayama <hazychill@gmail.com>
2018-08-07 04:02:33 +0900

    TimeBar

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace TimeBar
+{
+    /// <summary>
+    /// MainWindow.xaml の相互作用ロジック
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        DateTime start;
+        TimeSpan duration;
+        TimeBarOptions options;
+
+        DispatcherTimer timer;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            var args = Environment.GetCommandLineArgs().Skip(1).ToArray();
+            try
+            {
+                var parseResult = CommandLine.Parser.Default.ParseArguments<TimeBarOptions>(args) as CommandLine.Parsed<TimeBarOptions>;
+                if (parseResult != null)
+                {
+                    options = parseResult.Value;
+
+                    this.Width = options.Width;
+                    this.Height = options.Height;
+                    elapsedCanvas.Background = new SolidColorBrush(options.ColorValueElapsed);
+                    remainingCanvas.Background = new SolidColorBrush(options.ColorValueRemaining);
+                    frameBorde.BorderBrush = new SolidColorBrush(options.ColorValueBorder);
+                    var bgColor = Colors.White;
+                    bgColor.A = options.Transparency;
+                    start = DateTime.Now;
+                    if (options.EndTime != null)
+                    {
+                        var endTime = DateTime.Parse(options.EndTime);
+                        duration = endTime - start;
+                    }
+                    else
+                    {
+                        duration = TimeSpan.FromMinutes(options.Minutes);
+                    }
+                    var check = TimeSpan.MinValue.ToString(options.Format);
+                    this.Topmost = options.TopMost;
+                }
+                else
+                {
+                    MessageBox.Show($"Option Parse Error: {string.Join(" ", args.Select(x => $"\"{x}\""))}");
+                    this.Close();
+                }
+
+                timer = new DispatcherTimer();
+                timer.Interval = TimeSpan.FromSeconds(options.UpdateIntervalSec);
+                timer.Tick += (sender, e) => {
+                    var now = DateTime.Now;
+                    var elapsed = now - start;
+                    var elapsedWidth = (int)(this.Width * elapsed.Ticks / duration.Ticks);
+                    elapsedCanvas.Width = elapsedWidth;
+                    var remaining = start + duration - now;
+                    string format;
+                    if (remaining.Ticks >= 0)
+                    {
+                        format = options.Format;
+                    }
+                    else
+                    {
+                        format = $"\\-{options.Format}";
+                    }
+                    timeLabel.Content = remaining.ToString(format);
+                }; 
+                timer.Start();
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.ToString());
+                this.Close();
+            }
+        }
+
+        private void Window_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Left)
+            {
+                this.DragMove();
+            }
+        }
+    }
+}
---
+++ TimeBarOptions.cs
@@ -0,0 +1,77 @@
+﻿using CommandLine;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+
+namespace TimeBar
+{
+    class TimeBarOptions
+    {
+        [Option("EndTime")]
+        public string EndTime { get; set; }
+
+        [Option("Minutes", Default = 30)]
+        public int Minutes { get; set; }
+
+        [Option("ColorElapsed", Default = "LightGray")]
+        public string ColorElapsed { get; set; }
+
+        public Color ColorValueElapsed
+        {
+            get
+            {
+                var color = (Color)ColorConverter.ConvertFromString(ColorElapsed);
+                color.A = Transparency;
+                return color;
+            }
+        }
+
+        [Option("ColorRemaining", Default = "RoyalBlue")]
+        public string ColorRemaining { get; set; }
+
+        public Color ColorValueRemaining
+        {
+            get
+            {
+                var color = (Color)ColorConverter.ConvertFromString(ColorRemaining);
+                color.A = Transparency;
+                return color;
+            }
+        }
+
+
+        [Option("ColorBorder", Default = "Black")]
+        public string ColorBorder { get; set; }
+
+        public Color ColorValueBorder
+        {
+            get
+            {
+                var color = (Color)ColorConverter.ConvertFromString(ColorBorder);
+                color.A = Transparency;
+                return color;
+            }
+        }
+
+        [Option("Transparency", Default = (byte)100)]
+        public byte Transparency { get; set; }
+
+        [Option("Width", Default = 150)]
+        public int Width { get; set; }
+
+        [Option("Height", Default = 30)]
+        public int Height { get; set; }
+
+        [Option("Format", Default = "hh\\:mm\\:ss")]
+        public string Format { get; set; }
+
+        [Option("UpdateIntervalSec", Default = 1)]
+        public int UpdateIntervalSec { get; set; }
+
+        [Option("TopMost", Default = true)]
+        public bool TopMost { get; set; }
+    }
+}
